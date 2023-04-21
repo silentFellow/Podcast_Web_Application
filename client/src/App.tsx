af425f3e8f54762ab730aa9faeed3c73fd935676
theGreatestIntelligent <thegreatestintelligent@gmail.com>
2023-04-21 20:21:35 +0530

    setUp

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function App() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima officia maxime voluptates quo iure minus voluptate eum reiciendis, consectetur nihil deserunt possimus nemo, necessitatibus modi doloremque similique qui libero alias?
+    </div>
+  )
+}
+
+export default App
